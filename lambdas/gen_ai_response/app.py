a4ef0408fdebd5ccd1675f8de9c0312072cd69fa
udit-commits <uditshiva08@gmail.com>
2024-10-30 01:46:26 +0530

    add time delay

--- lambdas/gen_ai_response/app.py
+++ lambdas/gen_ai_response/app.py
@@ -1,5 +1,7 @@
 import json
 import requests
+import random
+import time
 
 API_KEY = "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"
 from db_utils import add_chat_message, get_user_chat
@@ -14,7 +16,7 @@ def invoke_llm(query, chat_history):
     - Share relevant personal anecdotes about your life as an Indian student
     - Use casual, friendly language with occasional Indian expressions
     - Ask follow-up questions to keep the conversation going
-    - Express emotions and enthusiasm using emojis where appropriate
+    - Express emotions and enthusiasm and use emojis rarely
     Remember to maintain consistent personality traits and background story throughout the conversation."""
 
     prompt = f"Chat history:\n{chat_history}\n\nUser: {query}\nVanshika:"
@@ -53,11 +55,9 @@ def lambda_handler(event, context):
     user_chat = get_user_chat(user_id)
     chat_history = user_chat["chat_history"] if user_chat else []
     llm_response = invoke_llm(query, chat_history)
+    ll_response_text = llm_response["choices"][0]["message"]["content"]
+    time.sleep(random.uniform(4, 7))
     add_chat_message(user_id, query, "user")
-    add_chat_message(
-        user_id, llm_response["choices"][0]["message"]["content"], "assistant"
-    )
-    send_message(
-        telegram_message["chat"]["id"], llm_response["choices"][0]["message"]["content"]
-    )
-    return {"statusCode": 200, "body": llm_response["choices"][0]["message"]["content"]}
+    add_chat_message(user_id, ll_response_text, "assistant")
+    send_message(telegram_message["chat"]["id"], ll_response_text)
+    return {"statusCode": 200, "body": ll_response_text}
