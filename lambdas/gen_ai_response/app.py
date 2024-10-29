11c08e9bd70cb66f010d36a40a64e5553b7b7bda
udit-commits <uditshiva08@gmail.com>
2024-10-30 00:04:57 +0530

    add gpt repsonse

--- lambdas/gen_ai_response/app.py
+++ lambdas/gen_ai_response/app.py
@@ -1,27 +1,28 @@
 import json
-
-
 import requests
+
+API_KEY = "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"
+
+
 def invoke_llm():
     response = requests.post(
-    url="https://openrouter.ai/api/v1/chat/completions",
-    headers={
-        "Authorization": f"Bearer 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",
-    },
-    data=json.dumps({
-        "model": "openai/gpt-4o-mini", # Optional
-        "messages": [
-        {
-            "role": "user",
-            "content": "What is the meaning of life?"
-        }
-        ]
-    })
+        url="https://openrouter.ai/api/v1/chat/completions",
+        headers={
+            "Authorization": f"Bearer {API_KEY}",
+        },
+        data=json.dumps(
+            {
+                "model": "openai/gpt-4o-mini",
+                "messages": [
+                    {"role": "user", "content": "What is the meaning of life?"}
+                ],
+                "max_tokens": 30,  # Limit response length to 30 tokens
+            }
+        ),
     )
-    return response
+    return response.json()
+
+
 def lambda_handler(event, context):
-    # print(invoke_llm())
-    return {
-        "statusCode": 200,
-        "body": json.dumps("Hello Im emotional AI assistant local change "),
-    }
+    llm_response = invoke_llm()
+    return {"statusCode": 200, "body": llm_response["choices"][0]["message"]["content"]}
